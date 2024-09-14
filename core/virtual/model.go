ea6857f9686c0c9b9055e7fc7428fb176d14dd8e
injoyai <1113655791@qq.com>
2024-09-14 16:04:44 +0800

    实现conv.Extend方便写代码

--- core/virtual/model.go
+++ core/virtual/model.go
@@ -3,14 +3,16 @@ package virtual
 import (
 	"encoding/json"
 	"github.com/injoyai/base/g"
+	"github.com/injoyai/conv"
 	"github.com/injoyai/proxy/core"
 )
 
 type RegisterReq struct {
-	Listen   *core.Listen `json:"listen,omitempty"`   //监听信息
-	Username string       `json:"username,omitempty"` //用户名
-	Password string       `json:"password,omitempty"` //密码
-	Param    g.Map        `json:"param,omitempty"`    //其他参数
+	Listen      *core.Listen `json:"listen,omitempty"`   //监听信息
+	Username    string       `json:"username,omitempty"` //用户名
+	Password    string       `json:"password,omitempty"` //密码
+	Param       g.Map        `json:"param,omitempty"`    //其他参数
+	conv.Extend `json:"-"`
 }
 
 func (this *RegisterReq) String() string {
