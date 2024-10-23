f5aaa1958f0ca5081fe05c23eb5eb80df1c6fa19
injoyai <1113655791@qq.com>
2024-10-23 17:08:52 +0800

    增加special包和对应的示例,用于只有一个隧道连接,然后只占用一个端口,隧道和请求端口为同一个

---
+++ example/special/server.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"github.com/injoyai/conv/cfg/v2"
+	"github.com/injoyai/logs"
+	"github.com/injoyai/proxy/core"
+	"github.com/injoyai/proxy/special"
+)
+
+func init() {
+	core.DefaultLog.SetLevel(core.LevelInfo)
+	cfg.Init(
+		cfg.WithYaml("./config/config.yaml"),
+		cfg.WithFlag(
+			&cfg.Flag{Name: "port", Default: 7001, Usage: "服务端口"},
+			&cfg.Flag{Name: "username", Default: "username", Usage: "允许注册的用户"},
+			&cfg.Flag{Name: "password", Default: "password", Usage: "允许注册的用户密码"},
+			&cfg.Flag{Name: "address", Default: ":80", Usage: "内网穿透的地址"},
+		),
+	)
+}
+
+func main() {
+
+	port := cfg.GetInt("port", 7001)
+	username := cfg.GetString("username", "username")
+	password := cfg.GetString("password", "password")
+	address := cfg.GetString("address", ":80")
+
+	s := special.New(
+		special.WithPort(port),
+		special.WithAddress(address),
+		special.WithRegister(func(tun *core.Tunnel, register *core.RegisterReqExtend) error {
+			if register.Username != username {
+				return fmt.Errorf("没有权限")
+			}
+			if register.Password != password {
+				return fmt.Errorf("没有权限")
+			}
+			return nil
+		}),
+	)
+	logs.Err(s.Run())
+
+}
---
+++ special/server.go
@@ -0,0 +1,124 @@
+package special
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"github.com/injoyai/logs"
+	"github.com/injoyai/proxy/core"
+	"io"
+	"net"
+)
+
+/*
+只有1个隧道连接
+隧道和代理共用一个端口
+
+*/
+
+type Option func(s *Server)
+
+func WithAddress(address string) Option {
+	return func(s *Server) {
+		s.Address = address
+	}
+}
+
+func WithPort(port int) Option {
+	return func(s *Server) {
+		s.Port = port
+	}
+}
+
+func WithRegister(onRegister func(tun *core.Tunnel, register *core.RegisterReqExtend) error) Option {
+	return func(s *Server) {
+		s.OnRegister = onRegister
+	}
+}
+
+func New(op ...Option) *Server {
+	s := &Server{
+		Port:       7001,
+		OnRegister: nil,
+	}
+	for _, v := range op {
+		v(s)
+	}
+	return s
+}
+
+type Server struct {
+	tunnel   *core.Tunnel
+	listener net.Listener
+
+	Port       int                                                            //服务监听的端口
+	Address    string                                                         //客户端转发的地址
+	OnRegister func(tun *core.Tunnel, register *core.RegisterReqExtend) error //注册事件
+}
+
+func (this *Server) Run() error {
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", this.Port))
+	if err != nil {
+		return err
+	}
+	core.DefaultLog.Infof("监听端口[:%d]成功...\n", this.Port)
+
+	this.listener = listener
+	for {
+		c, err := this.listener.Accept()
+		if err != nil {
+			return err
+		}
+		go func(c net.Conn) {
+			if err := this.handler(c); err != nil && err != io.EOF {
+				logs.Err(err)
+			}
+		}(c)
+	}
+}
+
+func (this *Server) handler(c net.Conn) error {
+	defer c.Close()
+
+	//读取前2字节是否是默认协议的帧头
+	prefix := make([]byte, 2)
+	n, err := io.ReadAtLeast(c, prefix, 2)
+	if err != nil {
+		return err
+	}
+
+	//说明是隧道连接
+	if n == 2 && prefix[0] == 0x89 && prefix[1] == 0x89 {
+		this.tunnel = core.NewTunnel(struct {
+			io.Reader
+			io.WriteCloser
+		}{io.MultiReader(bytes.NewReader(prefix), c), c})
+		this.tunnel.SetOption(
+			core.WithKey(c.RemoteAddr().String()),
+			core.WithRegister(func(tun *core.Tunnel, p core.Packet) (interface{}, error) {
+				register := new(core.RegisterReq)
+				err := json.Unmarshal(p.GetData(), register)
+				if err != nil {
+					return nil, err
+				}
+				//注册事件
+				if this.OnRegister != nil {
+					if err := this.OnRegister(tun, register.Extend()); err != nil {
+						return nil, err
+					}
+				}
+				return nil, nil
+			}))
+		return this.tunnel.Run()
+	}
+
+	if this.tunnel == nil || this.tunnel.Closed() {
+		return nil
+	}
+
+	core.DefaultLog.Infof("监听[:%d] -> 隧道[%s] -> 请求[%s]\n", this.Port, this.tunnel.Key(), this.Address)
+
+	//普通代理连接
+	return this.tunnel.DialAndSwap(c.RemoteAddr().String(), core.NewDialTCP(this.Address), c)
+
+}
